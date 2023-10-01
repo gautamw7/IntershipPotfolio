5a896ba4241a4314c62edb40a0d8e8fa365260bd
gautamw7 <141011220+gautamw7@users.noreply.github.com>
2023-10-01 16:47:48 +0530

    Update LanguageTable.java
    Working RadioButton

--- Project/GUI/LanguageTable.java
+++ Project/GUI/LanguageTable.java
@@ -1,66 +1,109 @@
-/*
-* Create a table for storing the table data
-* create table,, create radio group, create radio button */
-
-package Project.GUI;
-import Project.DatabaseConnection;
-
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+package Project;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.table.*;
+
 public class LanguageTable extends JFrame {
-    private DatabaseConnection dbConnection;
+    private DefaultTableModel dtm;
+    private ButtonGroup[] buttonGroups;
     private JTable table;
-    private JLabel TitleLabel, MessageLabel;
+    private JScrollPane jsp;
+
     public LanguageTable() {
-        setTitle("Language Proficiency Table");
-        setSize(800, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Language Proficiency  Test");
 
-        // Create a table model with 6 columns
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("Language");
-        model.addColumn("Very unfamiliar");
-        model.addColumn("Somewhat unfamiliar");
-        model.addColumn("Somewhat proficient");
-        model.addColumn("Very proficient");
-        model.addColumn("Expert Level");
-
-        // Define the rows with languages
+        // The languages String
         String[] languages = {
                 "Android (Java/Kotlin)", "C", "C++", "Rust", "Go",
                 "iOS (Swift/Objective-C)", "Java (Non-Android)", "JavaScript", "TypeScript", "Julia",
                 "PHP", "Python", "Ruby", "SQL (MySQL, PostgreSQL, etc.)", "Haskell", "OCaml"
         };
 
-        // Add rows to the table with radio buttons
+        String[] proficiencyLevel = {"Very unfamiliar", "Somewhat unfamiliar", "Somewhat proficient", "Very proficient", "Expert Level"};
+
+        DefaultTableModel dtm = new DefaultTableModel();
+        dtm.addColumn("Language");
+
+        // Add columns for proficiency levels
         for (String language : languages) {
-            Object[] row = new Object[6];
-            row[0] = language;
-
-            // Create a button group for each row
-            ButtonGroup buttonGroup = new ButtonGroup();
-
-            for (int i = 1; i <= 5; i++) {
-                JRadioButton radioButton = new JRadioButton();
-                buttonGroup.add(radioButton);
-                row[i] = radioButton;
-            }
-
-            model.addRow(row);
+            dtm.addColumn(language);
         }
 
-        table = new JTable(model);
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        // Add rows for each proficiency level with radio buttons
+        for (String level : proficiencyLevel) {
+            Object[] row = new Object[languages.length + 1];
+            row[0] = level;
+            for (int i = 1; i <= languages.length; i++) {
+                row[i] = new JRadioButton();
+            }
+            dtm.addRow(row);
+        }
 
-        // Create a scroll pane for the table
-        JScrollPane scrollPane = new JScrollPane(table);
-        getContentPane().add(scrollPane, BorderLayout.CENTER);
+        JTable table = new JTable(dtm) {
+            public void tableChanged(TableModelEvent tme) {
+                super.tableChanged(tme);
+                repaint();
+            }
+        };
 
+        buttonGroups = new ButtonGroup[languages.length];
+        for (int i = 0; i < languages.length; i++) {
+            buttonGroups[i] = new ButtonGroup();
+        }
+
+        // Assign radio buttons to button groups
+        for (int row = 0; row < dtm.getRowCount(); row++) {
+            for (int col = 1; col <= languages.length; col++) {
+                JRadioButton radioButton = (JRadioButton) dtm.getValueAt(row, col);
+                buttonGroups[col - 1].add(radioButton);
+            }
+        }
+
+        // Set cell renderer and editor for proficiency level columns
+        for (String language : languages) {
+            table.getColumn(language).setCellRenderer(new RadioButtonRenderer());
+            table.getColumn(language).setCellEditor(new RadioButtonEditor(new JCheckBox()));
+        }
+
+        JScrollPane jsp = new JScrollPane(table);
+        add(jsp, BorderLayout.NORTH);
+
+        setSize(800, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+}
+
+class RadioButtonRenderer implements TableCellRenderer {
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        if (value == null) return null;
+        return (Component) value;
+    }
+}
+
+class RadioButtonEditor extends DefaultCellEditor implements ItemListener {
+    private JRadioButton button;
+
+    public RadioButtonEditor(JCheckBox checkBox) {
+        super(checkBox);
+    }
+
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        if (value == null) return null;
+        button = (JRadioButton) value;
+        button.addItemListener(this);
+        return (Component) value;
+    }
+
+    public Object getCellEditorValue() {
+        button.removeItemListener(this);
+        return button;
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        super.fireEditingStopped();
     }
 }
